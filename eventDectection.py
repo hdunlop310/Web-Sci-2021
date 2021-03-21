7cc2f6f6073933cc196f77d71308663d2216c72f
Alana Grant <2390384g@student.gla.ac.uk>
2021-03-21 22:59:06 +0000

    eventDetect part 2 and reducedKeywords

--- eventDectection.py
+++ eventDectection.py
@@ -19,10 +19,13 @@ all_stopwords.extend(more_stop_words)
 
 with client:
     db = client["TwitterDump"]
-    collection = db.March21st.find({}, {'username': 1, 'text': 1, 'geoenabled':1})
+    collection = db.March21st.find({}, {'username': 1, 'text': 1, 'geoenabled':1, 'location':1, 'verified':1, 'media':1})
     tweets = []
     users = []
     geoenabled = []
+    locations = []
+    verified = []
+    media = []
 
     for tweet in collection:
         tweet['text'] = tweet['text'].lower()
@@ -33,6 +36,9 @@ with client:
         tweets.append(tweet['text'])
         users.append(tweet['username'])
         geoenabled.append(tweet['geoenabled'])
+        locations.append(tweet['location'])
+        verified.append(tweet['verified'])
+        media.append(tweet['media'])
 
 
 vectorizer = TfidfVectorizer(stop_words={'english'})
@@ -49,14 +55,15 @@ true_k = 5
 model = KMeans(n_clusters=true_k, init='k-means++', max_iter=200, n_init=10)
 model.fit(x)
 labels = model.labels_
-tweet_col = pd.DataFrame(list(zip(users, tweets, geoenabled, labels)), columns=['users', 'tweets', 'geoenabled', 'cluster'])
+tweet_col = pd.DataFrame(list(zip(users, tweets, geoenabled, locations, verified, media, labels)), columns=['users', 'tweets', 'geoenabled', 'locations', 'verified', 'media', 'cluster'])
 print(tweet_col.sort_values(by=['cluster']))
 
-result = {'cluster': labels, 'tweets': tweets, 'geoenabled': geoenabled}
+results = {'clusters': labels, 'tweets': tweets, 'users': users, 'geoenabled': geoenabled, 'locations': locations, 'verified': verified, 'media': media}
 
-result = pd.DataFrame(result)
+result = pd.DataFrame(results)
 
-k = 0
+'''
+k = 3
 s = result[result.cluster == k]
 text = s ['tweets'].str.cat(sep=' ')
 text = text.lower()
@@ -70,3 +77,35 @@ plt.figure()
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
+'''
+
+cluster0_locations = []
+for x in db.enhanced_cluster0.find({}, {'location': 1}):
+    if x['location'] != None:
+        cluster0_locations.append(x['location'])
+
+cluster1_locations = []
+for x in db.enhanced_cluster1.find({}, {'location': 1}):
+    if x['location'] != None:
+        cluster1_locations.append(x['location'])
+
+cluster2_locations = []
+for x in db.enhanced_cluster2.find({}, {'location': 1}):
+    if x['location'] != None:
+        cluster2_locations.append(x['location'])
+
+cluster3_locations = []
+for x in db.enhanced_cluster3.find({}, {'location': 1}):
+    if x['location'] != None:
+        cluster3_locations.append(x['location'])
+
+cluster4_locations = []
+for x in db.enhanced_cluster4.find({}, {'location': 1}):
+    if x['location'] != None:
+        cluster4_locations.append(x['location'])
+
+print(cluster0_locations)
+print(cluster1_locations)
+print(cluster2_locations)
+print(cluster3_locations)
+print(cluster4_locations)
